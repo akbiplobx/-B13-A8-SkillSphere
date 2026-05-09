94535338d91b7cf91ddeb6e92659aa781c584247
A K Biplob <akbiplobx@gmail.com>
2026-05-09 18:39:58 +0600

    proxy added

--- src/lib/auth-client.js
+++ src/lib/auth-client.js
@@ -2,5 +2,6 @@ import { createAuthClient } from "better-auth/react"
 export const authClient = createAuthClient({
    
     baseURL: "https://b13-a8-skill-sphere.vercel.app"
+    
 })
 export const { signIn, signUp, signOut, useSession } = createAuthClient();
