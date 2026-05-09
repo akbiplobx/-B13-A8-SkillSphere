2a24200a0428f22a1a1da14d20ce89aaf67dd438
A K Biplob <akbiplobx@gmail.com>
2026-05-09 11:26:07 +0600

    k

--- src/lib/auth-client.js
+++ src/lib/auth-client.js
@@ -1,6 +1,6 @@
 import { createAuthClient } from "better-auth/react"
 export const authClient = createAuthClient({
    
-    baseURL: "https://skill-sphere-ak.vercel.app"
+    baseURL: "https://b13-a8-skill-sphere.vercel.app"
 })
 export const { signIn, signUp, signOut, useSession } = createAuthClient();
